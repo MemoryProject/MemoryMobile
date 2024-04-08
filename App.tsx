db65e316caf7da442031156fb17e37e8c0e9e0df
Thayendran <kobikessava@gmail.com>
2024-04-08 11:15:05 +0200

    test

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>C'est kobi le chef de la class</Text>
       <StatusBar style="auto" />
     </View>
   );
